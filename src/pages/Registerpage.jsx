a5d02c03387ada76dd09035c748358ab2e3e3d39
Arbin1810 <arrbean1810@gmail.com>
2025-02-09 13:49:51 +0545

    register continue

--- src/pages/Registerpage.jsx
+++ src/pages/Registerpage.jsx
@@ -4,11 +4,12 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 const Registerpage = () => {
   return (
     <>
-      <Formik>
-        <div className="flex justify-center shadow-md shadow-blue-500">
-          <Form>
+      <div className="flex justify-center my-10">
+        <Formik>
+          <Form className=" shadow-lg shadow-blue-500 p-5">
             <div className="mb-2">
               <label htmlFor="fname">First Name</label>
+
               <Field
                 type="text"
                 className="rounded-lg block"
@@ -17,9 +18,53 @@ const Registerpage = () => {
               />
               <ErrorMessage name="fname" />
             </div>
+            <div className="mb-2">
+              <label htmlFor="lname">Last Name</label>
+
+              <Field
+                type="text"
+                className="rounded-lg block"
+                id="lname"
+                name="lname"
+              />
+              <ErrorMessage name="lname" />
+            </div>
+            <div className="mb-2">
+              <label htmlFor="email">Email</label>
+
+              <Field
+                type="email"
+                className="rounded-lg block"
+                id="email"
+                name="email"
+              />
+              <ErrorMessage name="email" />
+            </div>
+            <div className="mb-2">
+              <label htmlFor="pwd">Password</label>
+
+              <Field
+                type="password"
+                className="rounded-lg block"
+                id="pwd"
+                name="pwd"
+              />
+              <ErrorMessage name="pwd" />
+            </div>
+            <div className="mb-2">
+              <label htmlFor="cpwd">Confirm Password</label>
+
+              <Field
+                type="password"
+                className="rounded-lg block"
+                id="cpwd"
+                name="cpwd"
+              />
+              <ErrorMessage name="cpwd" />
+            </div>
           </Form>
-        </div>
-      </Formik>
+        </Formik>
+      </div>
 
       {/* <div>
         <h1>Any place in your app!</h1>
